1ec034986dcab24ecf823eac276b7ec58d708428
mohansabar <30342103+mohansabar@users.noreply.github.com>
2017-08-11 04:47:26 -0700

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
- 
 void main()
 {
-    int num;
- 
-    printf("Enter a number: \n");
-    scanf("%d", &num);
-    if (num > 0)
-        printf("%d is a positive number \n", num);
-    else if (num < 0)
-        printf("%d is a negative number \n", num);
+    int n;
+     printf("Enter a number: \n");
+    scanf("%d", &n);
+    if (n > 0)
+        printf("%d is a positive number \n", n);
+    else if (n < 0)
+        printf("%d is a negative number \n", n);
     else
         printf("0 is neither positive nor negative");
 }
